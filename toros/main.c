c0f273f369bb07701d79881177b7e3459df03bd0
vkhristenko <vdkhristenko1991@gmail.com>
2018-06-07 13:30:27 +0200

    building up to dumping root file

--- toros/main.c
+++ toros/main.c
@@ -1,68 +1,15 @@
 #include <stdio.h>
+#include <inttypes.h>
 #include <stdint.h>
 #include <string.h>
 #include <assert.h>
 #include <stdlib.h>
 
 //
-// product structs
+// file context
 //
-struct PObject {};
-struct PDatime {
-    struct PObject object;
-    uint32_t raw;
-};
-
-struct PFileHeader {
-    struct PObject object;
-    uint32_t version;
-    uint32_t begin;
-    uint32_t end;
-    uint64_t seek_free;
-    uint32_t nbytes_free;
-    uint32_t nfree;
-    uint32_t nbytes_name;
-    char units;
-    uint32_t compress;
-    uint64_t seek_info;
-    uint32_t nbytes_info;
-};
-
-struct PKey {
-    struct PObject object;
-    uint32_t total_bytes;
-    int32_t version;
-    uint32_t obj_bytes;
-    struct PDatime date_time;
-    uint16_t cycle;
-    uint64_t seek_key;
-    uint64_t seek_pdir;
-    char *class_name;
-    char *obj_name;
-    char *obj_title;
-};
-
-struct PDirectory {
-    struct PObject object;
-    struct PDatime date_time_c;
-    struct PDatime date_time_m;
-    uint32_t nbytes_keys;
-    uint32_t nbytes_name;
-    uint64_t seek_dir;
-    uint64_t seek_parent;
-    uint64_t seek_keys;
-};
-
 struct FileContext {
     FILE *pfile;
-};
-
-//
-// buffer functionality
-//
-struct Buffer {
-    size_t nbytes;
-    char *buffer;
 };
 
 void swap_16(char *src, char *dest) {
@@ -126,9 +73,35 @@ float get_f32(char *ptr) {
     return (float)get_u32(ptr);
 }
 
+uint32_t get_version(char *ptr) {
+    uint16_t v = get_u16(ptr); ptr+=2;
+    if ((v & 0x4000) == 0) 
+        return (uint32_t)v;
+
+    uint32_t nbytes = ((v & 0x3fff) << 16) + get_u16(ptr); ptr+=2;
+    uint16_t res = get_u16(ptr); ptr+=2;
+    return res;
+}
+
+/*
+void get_string(char *src, char *dest) {
+    uint8_t size = (uint8_t)*ptr; ptr++;
+    if (size == 0)
+        dest = "";
+    else if (size == -1) {
+        uint32_t size = get_u32(ptr); ptr+=4;
+        dest = 
+        memcpy(dest, src, size);
+    } else 
+}*/
+
+// 
+// bdebug stuff
 //
-// various printing of buffers
-//
+struct Buffer {
+    size_t nbytes;
+    char *buffer;
+};
 void dump_buffer(struct Buffer buf, int nbytes_per_row) {
     printf("dumping buffer of size = %lu\n", buf.nbytes);
     for (size_t i=0; i<nbytes_per_row*3; i++)
@@ -141,7 +114,6 @@ void dump_buffer(struct Buffer buf, int nbytes_per_row) {
     }
     printf("\n");
 }
-
 void dump_raw(char *buf, int size, int nbytes_per_row) {
     printf("dumping buffer of size = %d\n", size);
     for (size_t i=0; i<nbytes_per_row*3; i++)
@@ -156,9 +128,81 @@ void dump_raw(char *buf, int size, int nbytes_per_row) {
 }
 
 //
-// builder functions
+// some macros
 //
-void build_file_header(struct Buffer buffer) {
+
+#define print_char(value) \
+    printf(#value" = %c\n", value)
+
+#define print_u32(value) \
+    printf(#value" = %u\n", value)
+
+#define print_u64(value) \
+    printf(#value" = %"PRIu64"\n", value)
+
+#define print_i32(value) \
+    printf(#value" = %d\n", value)
+
+#define print_i64(value) \
+    printf(#value" = %"PRId64"\n", value)
+
+#define print_f32(value) \
+    printf(#value" = %f\n", value)
+
+#define print_f64(value) \
+    printf(#value" = %lf\n", value)
+
+
+//
+// product structs
+//
+struct PObject {};
+struct PDatime {
+    struct PObject object;
+    uint32_t raw;
+};
+
+struct PDatime build_datime(char *buffer) {
+    struct PDatime datime;
+    datime.raw = get_u32(buffer); buffer+=4;
+    return datime;
+}
+
+//
+// file header 
+//
+struct PFileHeader {
+    struct PObject object;
+    uint32_t version;
+    uint32_t begin;
+    uint64_t end;
+    uint64_t seek_free;
+    uint32_t nbytes_free;
+    uint32_t nfree;
+    uint32_t nbytes_name;
+    char units;
+    uint32_t compress;
+    uint64_t seek_info;
+    uint32_t nbytes_info;
+};
+
+void print_file_header(struct PFileHeader *pheader) {
+    printf("Product <PFileHeader>:\n");
+    printf("-----------------------\n");
+    print_u32(pheader->version);
+    print_u32(pheader->begin);
+    print_u64(pheader->end);
+    print_u64(pheader->seek_free);
+    print_u32(pheader->nbytes_free);
+    print_u32(pheader->nfree);
+    print_u32(pheader->nbytes_name);
+    print_char(pheader->units);
+    print_u32(pheader->compress);
+    print_u64(pheader->seek_info);
+    print_u32(pheader->nbytes_info);
+}
+
+struct PFileHeader build_file_header(struct Buffer buffer) {
     if (strncmp(buffer.buffer, "root", 4)) {
         printf("not a root file\n");
         assert(0);
@@ -166,17 +210,62 @@ void build_file_header(struct Buffer buffer) {
     buffer.buffer+=4;
     dump_buffer(buffer, 20);
 
-    uint32_t version = get_u32(buffer.buffer);
-    printf("version = %d\n", version);
+    struct PFileHeader header;
+
+    header.version = get_u32(buffer.buffer);
+    printf("version = %d\n", header.version);
     buffer.buffer+=4;
     dump_buffer(buffer, 20);
 
-    uint32_t begin = get_u32(buffer.buffer);
-    printf("begin = %d\n", begin);
+    header.begin = get_u32(buffer.buffer);
+    printf("begin = %d\n", header.begin);
     buffer.buffer+=4;
 
-    return;
+    if (header.version > 1000000u) {
+        header.end = get_u64(buffer.buffer); buffer.buffer+=8;
+        header.seek_free = get_u64(buffer.buffer); buffer.buffer+=8;
+    }
+    header.nbytes_free = get_u32(buffer.buffer); buffer.buffer+=4;
+    header.nfree = get_u32(buffer.buffer); buffer.buffer+=4;
+    header.nbytes_name = get_u32(buffer.buffer); buffer.buffer+=4;
+    header.units = *(buffer.buffer); buffer.buffer++;
+    header.compress = get_u32(buffer.buffer); buffer.buffer+=4;
+    header.seek_info = get_u64(buffer.buffer); buffer.buffer+=8;
+    header.nbytes_info = get_u32(buffer.buffer); buffer.buffer+=4;
+
+    return header;
 }
+
+//
+// key product
+//
+struct PKey {
+    struct PObject object;
+    uint32_t total_bytes;
+    int32_t version;
+    uint32_t obj_bytes;
+    struct PDatime date_time;
+    uint16_t cycle;
+    uint64_t seek_key;
+    uint64_t seek_pdir;
+    char *class_name;
+    char *obj_name;
+    char *obj_title;
+};
+
+//
+// directory product
+//
+struct PDirectory {
+    struct PObject object;
+    struct PDatime date_time_c;
+    struct PDatime date_time_m;
+    uint32_t nbytes_keys;
+    uint32_t nbytes_name;
+    uint64_t seek_dir;
+    uint64_t seek_parent;
+    uint64_t seek_keys;
+};
 
 //
 // some logic
@@ -189,7 +278,8 @@ void get_top_dir(struct FileContext ctx) {
     printf("read nbytes = %lu\n", nbytes);
     dump_buffer(buf, 20);
 
-    build_file_header(buf);
+    struct PFileHeader header = build_file_header(buf);
+    print_file_header(&header);
 }
 
 void debug(struct FileContext ctx) {
